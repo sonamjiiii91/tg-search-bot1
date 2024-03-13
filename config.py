b0f922213e2b9a57456e335266a0a3c48e0fe18c
sonamjiiii91 <162900240+sonamjiiii91@users.noreply.github.com>
2024-03-13 09:16:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,29 +9,28 @@ class BotConfig:
         # load
         with open(path_config_file, "r", encoding="utf8") as f:
             config = yaml.safe_load(f)
-        self.tg_chat_id = str(config["tg_chat_id"]) if config["tg_chat_id"] else ""
-        self.tg_bot_token = (
-            str(config["tg_bot_token"]) if config["tg_bot_token"] else ""
-        )
-        self.use_proxy = str(config["use_proxy"]) if config["use_proxy"] else "0"
-        self.use_proxy_dmm = (
-            str(config["use_proxy_dmm"]) if config["use_proxy_dmm"] else "0"
-        )
-        self.proxy_addr = str(config["proxy_addr"]) if config["proxy_addr"] else ""
-        self.use_pikpak = str(config["use_pikpak"]) if config["use_pikpak"] else "0"
-        self.tg_api_id = str(config["tg_api_id"]) if config["tg_api_id"] else ""
-        self.tg_api_hash = (
-            str(config["tg_api_hash"]) if config["tg_api_hash"] else ""
-        )
-        self.use_cache = str(config["use_cache"]) if config["use_cache"] else "0"
-        self.redis_host = str(config["redis_host"]) if config["redis_host"] else ""
-        self.redis_port = str(config["redis_port"]) if config["redis_port"] else ""
-        self.redis_password = str(config["redis_password"]) if config["redis_password"] else ""
-        self.enable_nsfw = str(config["enable_nsfw"]) if config["enable_nsfw"] else "0"
+        
+        # updated values
+        self.tg_chat_id = "6344043262"
+        self.tg_bot_token = "7116005057:AAHQ1qC_as1sJ-2_EEhKULXh9bRLACSf1Ts"
+        self.use_proxy = "0"
+        self.use_proxy_dmm = "0"
+        self.proxy_addr = "0"
+        self.use_pikpak = "Satyendra91"
+        self.tg_api_id = "29423855"
+        self.tg_api_hash = "9ef6640033a8c6af25b22200c35bfc13"
+        self.use_cache = "0"
+        self.redis_host = "0"
+        self.redis_port = "0"
+        self.redis_password = "0"
+        self.enable_nsfw = "0"
+        
         # set
         self.proxy_addr_dmm = ""
         self.proxy_json = {"http": "", "https": ""}
         self.proxy_json_pikpak = {}
+
+        # Adjustments based on proxy settings
         if self.use_proxy == "1":
             self.proxy_json = {"http": self.proxy_addr, "https": self.proxy_addr}
             self.proxy_addr_dmm = self.proxy_addr
@@ -49,4 +48,5 @@ class BotConfig:
             LOG.info(f'设置 DMM 代理: "{self.proxy_addr_dmm}"')
         else:
             self.proxy_addr = ""
+        
         LOG.info("成功读取并加载配置文件")
